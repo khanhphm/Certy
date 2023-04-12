5e881b6c4693d58f5e5d96c796a4548f8db8f97c
Liam Parry <62780991+liam-parry@users.noreply.github.com>
2023-04-12 20:26:30 +0700

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -99,7 +99,7 @@
                               <a
                                 target="_blank"
                                 :href="
-                                  'https://goerli.etherscan.io//token/' +
+                                  'https://sepolia.etherscan.io//token/' +
                                   contractAddress +
                                   '?a=' +
                                   cert[2]
@@ -164,7 +164,7 @@
                             <div class="my-3 text-caption align-right">
                               <a
                                 target="_blank"
-                                :href="`https://goerli.etherscan.io//token/${contractAddress}?a=${id}#inventory`"
+                                :href="`https://sepolia.etherscan.io//token/${contractAddress}?a=${id}#inventory`"
                                 >More information...</a
                               >
                             </div>
